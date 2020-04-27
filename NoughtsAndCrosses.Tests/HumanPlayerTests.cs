c097d51f8b1d8f3018f4ef4b07d987fda7d1fa36
37407 <16180129+37407@users.noreply.github.com>
2020-04-27 21:07:24 +0100

    Added test project

---
+++ NoughtsAndCrosses.Tests/HumanPlayerTests.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace NoughtsAndCrosses.Tests
+{
+    public class HumanPlayerTests
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ NoughtsAndCrosses.Tests/TestConsoleInputRetriever.cs
@@ -0,0 +1,13 @@
+﻿namespace NoughtsAndCrosses.Tests
+{
+    public class TestConsoleInputRetriever : IConsoleInputRetriever
+    {
+        private readonly string _input;
+
+        public TestConsoleInputRetriever(string input) => _input = input;
+
+        public string UserInputReadLine() => _input;
+
+        public bool UserInputReadKey() => false;
+    }
+}
