18a7be49ef7424d583dd90b7a0275dce281c0c61
37407 <16180129+37407@users.noreply.github.com>
2020-05-02 10:20:56 +0100

    EasyComputerPlayerTests added

--- NoughtsAndCrosses.Tests/EasyComputerPlayerTests.cs
+++ NoughtsAndCrosses.Tests/EasyComputerPlayerTests.cs
@@ -1,6 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Collections.Generic;
+using System.Linq;
 using Xunit;
 
 namespace NoughtsAndCrosses.Tests
@@ -8,9 +7,41 @@ namespace NoughtsAndCrosses.Tests
     public class EasyComputerPlayerTests
     {
         [Fact]
-        public void EasyComputerPlayer_ReturnsBoard_OneSquareStateMatchesComputerPlayerSymbol()
+        public void EasyComputerPlayer_PlayerInput_ReturnsBoard_OneDisplayCharacterMatchesComputerPlayerSymbol()
         {
+            var board = new BoardCreator().Board;
+            var player = new EasyComputerPlayer();
+            var actual = player.PlayerInput(board);
 
+            var actualSquareSymbols = new List<string>();
+
+            foreach (var square in actual)
+            {
+                actualSquareSymbols.Add(square.DisplayCharacter);
+            }
+
+            Assert.Equal(9, actualSquareSymbols.Count);
+            Assert.Contains("O", actualSquareSymbols);
+            Assert.Single(actualSquareSymbols.Where(x => x.Equals("O")));
+        }
+
+        [Fact]
+        public void EasyComputerPlayer_PlayerInput_ReturnsBoard_OneSquareStateMatchesComputerPlayerState()
+        {
+            var board = new BoardCreator().Board;
+            var player = new EasyComputerPlayer();
+            var actual = player.PlayerInput(board);
+
+            var actualSquareStates = new List<SquareState>();
+
+            foreach (var square in actual)
+            {
+                actualSquareStates.Add(square.State);
+            }
+
+            Assert.Equal(9, actualSquareStates.Count);
+            Assert.Contains(SquareState.O, actualSquareStates);
+            Assert.Single(actualSquareStates.Where(x => x.Equals(SquareState.O)));
         }
     }
 }
